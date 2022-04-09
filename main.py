0354541e94ac82056aaeeb152723a396f56a50d7
alexanderthinh <alexanderthinh@gmail.com>
2022-04-09 15:34:56 +0700

    Khoi tao

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def happy_new_year_2022():
+    print('Hello World!')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    happy_new_year_2022()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
